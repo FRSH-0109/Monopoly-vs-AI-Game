c50c96a5e629fd654ed25a0173bca6e1ec3cacd6
FRSH-0109 <kamilkosnik@gmail.com>
2023-12-12 23:40:32 +0100

    First test added

--- Monopoly/tests/monopolyTests.cpp
+++ Monopoly/tests/monopolyTests.cpp
@@ -1,8 +1,17 @@
 #include "catch2/src/catch2/catch_all.hpp"
+// #include "SFML/Graphics.hpp"
+#include "../gameEngine.h"
 
 using namespace std;
 
 TEST_CASE("Initial tests")
 {
     CHECK(true);
+}
+
+TEST_CASE("Test of Game Engine constructor")
+{
+    GameEngine testEngine = GameEngine(30, 1000, 1200);
+    REQUIRE(testEngine.getWindowWidth() == 1000);
+    REQUIRE(testEngine.getWindowHeight() == 1200);
 }
