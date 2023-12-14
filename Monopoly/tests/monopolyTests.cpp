e7cd677ba8c5d27f26703ccc523b2a78a46146c0
kradzik1 <01168911@pw.edu.pl>
2023-12-14 14:42:55 +0100

    Changed test formatting using SECTION

--- Monopoly/tests/monopolyTests.cpp
+++ Monopoly/tests/monopolyTests.cpp
@@ -15,77 +15,63 @@
 
 using namespace std;
 
-TEST_CASE("Initial tests")
-{
-    CHECK(true);
-}
-
-TEST_CASE("GameEngine constructor")
+TEST_CASE("GameEngine class")
 {
     GameEngine testEngine = GameEngine(30, 1000, 1200);
+
     REQUIRE(testEngine.getWindowWidth() == 1000);
     REQUIRE(testEngine.getWindowHeight() == 1200);
+
+    SECTION("GameEngine getContextWindow()")
+    {
+        REQUIRE(testEngine.getContextWindow() == ContextWindow::GetInstance());
+    }
 }
 
 TEST_CASE("ContextWindow Singleton design pattern")
 {
     ContextWindow* contextWindow = ContextWindow::GetInstance();
     ContextWindow* newContextWindow = ContextWindow::GetInstance();
+
     REQUIRE(contextWindow == newContextWindow);
 }
 
-TEST_CASE("GameEngine getContextWindow()")
-{
-    GameEngine testEngine = GameEngine(30, 1000, 1200);
-    REQUIRE(testEngine.getContextWindow() == ContextWindow::GetInstance());
-}
-
-TEST_CASE("ActiveScreen getContextWindow")
+TEST_CASE("ActiveScreen classes")
 {
     MainMenuScreen mainMenu = MainMenuScreen();
     GameMenuScreen gameMenu = GameMenuScreen();
-    ContextWindow* contextWindow = ContextWindow::GetInstance();
-    mainMenu.setContextWindow(contextWindow);
-    gameMenu.setContextWindow(contextWindow);
-    REQUIRE(mainMenu.getContextWindow() == contextWindow);
-    REQUIRE(gameMenu.getContextWindow() == contextWindow);
-}
 
-TEST_CASE("MainMenuScreen constructor")
-{
-    MainMenuScreen mainMenu = MainMenuScreen();
     REQUIRE(mainMenu.getScreenType() == MainMenu);
-}
-
-TEST_CASE("GameMenuScreen constructor")
-{
-    GameMenuScreen gameMenu = GameMenuScreen();
     REQUIRE(gameMenu.getScreenType() == GameMenu);
+
+    SECTION("ActiveScreen getContextWindow")
+    {
+        ContextWindow* contextWindow = ContextWindow::GetInstance();
+        mainMenu.setContextWindow(contextWindow);
+        gameMenu.setContextWindow(contextWindow);
+
+        REQUIRE(mainMenu.getContextWindow() == contextWindow);
+        REQUIRE(gameMenu.getContextWindow() == contextWindow);
+    }
 }
 
-TEST_CASE("monopolyGameEngine constructor")
+TEST_CASE("monopolyGameEngine")
 {
     monopolyGameEngine monopolyEngine = monopolyGameEngine();
     REQUIRE(monopolyEngine.getPlayersHumanNumber() == 1);
     REQUIRE(monopolyEngine.getPlayersAINumber() == 1);
-}
 
-TEST_CASE("monopolyGameEngine setPlayersHumanNumber()")
-{
-    monopolyGameEngine monopolyEngine = monopolyGameEngine();
-    REQUIRE(monopolyEngine.getPlayersHumanNumber() == 1);
-    REQUIRE(monopolyEngine.getPlayersAINumber() == 1);
-    monopolyEngine.setPlayersHumanNumber(3);
-    REQUIRE(monopolyEngine.getPlayersHumanNumber() == 3);
-    REQUIRE(monopolyEngine.getPlayersAINumber() == 1);
-}
+    SECTION("monopolyGameEngine setPlayersHumanNumber()")
+    {
+        monopolyEngine.setPlayersHumanNumber(3);
+        REQUIRE(monopolyEngine.getPlayersHumanNumber() == 3);
+        REQUIRE(monopolyEngine.getPlayersAINumber() == 1);
+    }
 
-TEST_CASE("monopolyGameEngine setPlayersAINumber()")
-{
-    monopolyGameEngine monopolyEngine = monopolyGameEngine();
-    REQUIRE(monopolyEngine.getPlayersHumanNumber() == 1);
-    REQUIRE(monopolyEngine.getPlayersAINumber() == 1);
-    monopolyEngine.setPlayersAINumber(2);
-    REQUIRE(monopolyEngine.getPlayersHumanNumber() == 1);
-    REQUIRE(monopolyEngine.getPlayersAINumber() == 2);
+    SECTION("monopolyGameEngine setPlayersAINumber()")
+    {
+        monopolyEngine.setPlayersAINumber(2);
+        REQUIRE(monopolyEngine.getPlayersHumanNumber() == 1);
+        REQUIRE(monopolyEngine.getPlayersAINumber() == 2);
+    }
 }
