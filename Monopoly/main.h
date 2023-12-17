594f9b52638e744f8f3fdd58a058c222d993922e
kradzik1 <01168911@pw.edu.pl>
2023-12-17 13:29:32 +0100

    Added enum types needed in Field class

--- Monopoly/main.h
+++ Monopoly/main.h
@@ -15,4 +15,26 @@ enum ScreenEventType {
 	StartGame,
 };
 
+enum FieldType {
+	PROPERTY,
+	STATION,
+	UTILITY,
+	GO,
+	CHANCE,
+	COMMUNITY_CHEST,
+	TAX,
+	JAIL,
+	FREE_PARKING,
+	GO_TO_JAIL
+};
+
+enum PropertyTiers {
+	NO_HOUESES,
+	ONE_HOUSE,
+	TWO_HOUESES,
+	THREE_HOUSES,
+	FOUR_HOUSES,
+	HOTEL
+};
+
 #endif
