136ca8d804b5affb457a5df8f6ed0b9846391998
FRSH-0109 <kamilkosnik@gmail.com>
2023-12-29 12:12:26 +0100

    Added first button on GameScreen

--- Monopoly/GameScreen.cpp
+++ Monopoly/GameScreen.cpp
@@ -4,9 +4,18 @@ GameScreen::GameScreen(std::vector<std::shared_ptr<playerSettings>> playerSettin
 	setContextWindow(ContextWindow::GetInstance());
 	std::cout << "GS constructor" << std::endl;
 	setScreenType(MonopolyGame);
+
+	// TODO: exception handling
+	if (!getFont().loadFromFile("textures_and_fonts/fonts/Kabel-Heavy.ttf"))
+		std::cout << "Font not found!\n";
+
+	setFont(getFont());
+
 	monopoly_game_engine_.clearPlayers();
 	monopoly_game_engine_.createPlayers(playerSettingsList_);
 	monopoly_game_engine_.createBoard();
+
+	createButtonRollDice();
 }
 
 GameScreen::~GameScreen() {
@@ -15,19 +24,50 @@ GameScreen::~GameScreen() {
 
 ScreenEventType GameScreen::worker() {
 	ScreenEventType eventType = Idle;
+	for (auto element : getButtons()) {
+		if (element->getIsVisible()) {
+			if (element->getIsClicked()) {
+				// setOtherButtonsInactive(element);
+				// buttonClickHandle(element);
+				element->setIsActive(true);
+				element->setIsClicked(false);
+				return element->getEventType();
+			}
+		}
+
+		buttonSetColors(element);
+	}
 	return eventType;
 }
 
 void GameScreen::pollForEvents(sf::Event& event) {
 	switch (event.type) {
 		case sf::Event::MouseMoved:
+			for (auto element : getButtons()) {
+				if (element->isMouseOver(getContextWindow()->getWindow())) {
+					element->mouseIsOver();
+				} else {
+					element->mouseIsNotOver();
+				}
+			}
 			break;
 		case sf::Event::MouseButtonPressed:
+			for (auto element : getButtons()) {
+				if (element->isMouseOver(getContextWindow()->getWindow())) {
+					element->setIsClicked(true);
+				}
+			}
 			break;
 	}
 }
 
 void GameScreen::draw() {
+	for (auto element : getButtons()) {
+		if (element->getIsVisible()) {
+			element->draw(getContextWindow()->getWindow());
+		}
+	}
+
 	float x_scaled;
 	float y_sclaed;
 	sf::Vector2u texture_origin;
@@ -45,5 +85,39 @@ void GameScreen::draw() {
 		y_sclaed = round((float)texture_origin.y * texture_scale.y);
 		pos_vect.x += dir_x * x_scaled;
 		pos_vect.y += dir_y * y_sclaed;
+	}
+}
+
+void GameScreen::createButtonRollDice()
+{
+	int fontSize = 30;
+	sf::Vector2f buttonSize = sf::Vector2f(120, 60);
+	sf::Color activeButtonBackColor = sf::Color::Green;
+	sf::Color inActiveButtonBackColor = sf::Color(192,192,192); //GREY
+	sf::Color FocusButtonBackColor = sf::Color::Black;
+	sf::Color activeButtonTextColor = sf::Color::Black;
+	sf::Color inActiveButtonTextColor = sf::Color::Black;
+	sf::Color FocusButtonTextColor = sf::Color::Green;
+	std::shared_ptr<Button> buttonRollDice(new Button(Idle, "Roll Dice", buttonSize, fontSize));
+	buttonRollDice->setFont(getFont());
+	buttonRollDice->setPosition(sf::Vector2f(500, 500));
+	buttonRollDice->setActiveBackColor(activeButtonBackColor);
+	buttonRollDice->setActiveTextColor(activeButtonTextColor);
+	buttonRollDice->setInactiveBackColor(inActiveButtonBackColor);
+	buttonRollDice->setInactiveTextColor(inActiveButtonTextColor);
+	buttonRollDice->setFocusBackColor(FocusButtonBackColor);
+	buttonRollDice->setFocusTextColor(FocusButtonTextColor);
+	buttonRollDice->setIsClicked(false);
+	buttonRollDice->setIsVisible(true);
+	buttonRollDice->setIsActive(false);
+	buttonRollDice->setIsFocus(false);
+	addButton(buttonRollDice);
+}
+
+void GameScreen::setOtherButtonsInactive(std::shared_ptr<Button> buttonPtr) {
+	for (auto element : getButtons()) {
+		if (element != buttonPtr) {
+			element->setIsActive(false);
+		}
 	}
 }
